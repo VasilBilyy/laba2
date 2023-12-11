e8939b8daa57e0a4d30ab92347361f53fa84e0e9
VasilBilyy <bilyjvasil@gmail.com>
2023-12-11 23:54:17 +0200

    реалізація Main згідно вимог

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,62 @@
 package org.example;
-import java.util.ArrayList;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+enum SortingType {
+    BUBBLE, SHELL, MERGE, QUICK
+}
+
+class SortingFactory {
+    public static Sorter getSorter(SortingType type) {
+        switch (type) {
+            case BUBBLE:
+                return new BubbleSorter();
+            case SHELL:
+                return new ShellSorter();
+            case MERGE:
+                return new MergeSorter();
+            case QUICK:
+                return new QuickSorter();
+            default:
+                throw new IllegalArgumentException("Unsupported sorting type: " + type);
+        }
+    }
+}
+
 public class Main {
     private static Random random = new Random();
     public static void main(String[] args) {
+        for (int count : new int[]{10, 1000, 10000, 1000000}) {
+            System.out.println("Number of elements: " + count);
 
-        QuickSorter quickSorter = new QuickSorter();
+            ArrayList<Integer> inputArray = generateRandomArray(count);
+            //printArray(inputArray);
 
-        ArrayList<Integer> inputList = new ArrayList<>();
+            for (SortingType type : SortingType.values()) {
+                System.out.println("Sorting type: " + type);
 
-        for (int i = 0; i < 10; i++){
-            inputList.add(random.nextInt(10));
+                Sorter sorter = SortingFactory.getSorter(type);
+                long startTime = System.currentTimeMillis();
+                ArrayList<Integer> sortedArray = sorter.sort(new ArrayList<>(inputArray));
+                long endTime = System.currentTimeMillis();
+
+                printArray(sortedArray.subList(0, Math.min(50, count)));
+                System.out.println("Time taken: " + (endTime - startTime) + " milliseconds\n");
+            }
         }
+    }
 
-        System.out.println("Початковий масив: " + inputList);
+    private static ArrayList<Integer> generateRandomArray(int count) {
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            array.add(random.nextInt(count));
+        }
+        return array;
+    }
 
-        ArrayList<Integer> sortedList = quickSorter.sort(inputList);
-
-        System.out.println("Відсортований масив: " + sortedList);
+    private static void printArray(List<Integer> array) {
+        System.out.println(Arrays.toString(array.toArray(new Integer[0])));
     }
 }
