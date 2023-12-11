902724c9342fdecffe3f1416482971432ef9c739
VasilBilyy <bilyjvasil@gmail.com>
2023-12-11 23:44:48 +0200

    перевірена реалізація класу BubbleSorter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Random;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static Random random = new Random();
     public static void main(String[] args) {
 
+        BubbleSorter bubbleSorter = new BubbleSorter();
+
+        ArrayList<Integer> inputList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            inputList.add(random.nextInt(10));
+        }
+
+        System.out.println("Початковий масив: " + inputList);
+
+        ArrayList<Integer> sortedList = bubbleSorter.sort(inputList);
+
+        System.out.println("Відсортований масив: " + sortedList);
     }
 }
